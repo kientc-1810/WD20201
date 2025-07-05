f3e63bd36fb81bca7d29dc1b2fbe0c56fccedc1d
kientc <kientc@fpt.edu.vn>
2025-07-05 12:45:30 +0700

    Học cách sử dụng vue3

---
+++ src/components/SlideVue1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 class="display-4 mb-3">Thông tin khóa học:</h1>
+    <ul class="list-group">
+      <li class="list-group-item">Tên môn học: {{ name }}</li>
+      <li class="list-group-item">Mã môn: {{ idCode }}</li>
+      <li class="list-group-item">Mã lớp: {{ idClass }}</li>
+      <li class="list-group-item">Trạng thái: {{ active ? "Mở" : "Đóng" }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const name = "Frontend-framework";
+const idCode = "WEB2072";
+const idClass = "WD20201";
+const active = "true";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Xin chào, lớp ăn trưa chưa?</h1>
+  </main>
+</template>
